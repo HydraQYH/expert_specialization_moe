c56c28434bd146e064b5d5a3ec6c9f035e0590e9
戚余航 <qiyuhang@bytedance.com>
2025-10-24 08:46:42 +0800

    Fix h20 middle config.

--- expert_specialization/csrc/cuda/es_fp8_blockwise_launcher.cuh
+++ expert_specialization/csrc/cuda/es_fp8_blockwise_launcher.cuh
@@ -231,7 +231,7 @@ void es_sm90_fp8_blockwise_scaled_group_mm_distpatch_out_dtype(
         mm_problem_sizes,
         stream);
   } else {
-    launch_sm90_fp8_blockwise_scaled_group_mm<HighMGemmHx00Traits>(
+    launch_sm90_fp8_blockwise_scaled_group_mm<MiddleMGemmH20Traits>(
         out_ptrs,
         a_ptrs,
         b_ptrs,
