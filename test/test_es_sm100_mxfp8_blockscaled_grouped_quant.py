2d26bfe9291f069293ce1ec452a79c648bef0c8a
戚余航 <qiyuhang@bytedance.com>
2025-11-20 14:26:09 +0800

    Refine quant kernel unitest.

--- test/test_es_sm100_mxfp8_blockscaled_grouped_quant.py
+++ test/test_es_sm100_mxfp8_blockscaled_grouped_quant.py
@@ -2,13 +2,7 @@ import random
 from typing import Tuple
 import pytest
 import torch
-import tensorrt_llm
-from expert_specialization.ops import es_sm100_mxfp8_blockscaled_grouped_quant
-
-seed = 77
-torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
+from expert_specialization.ops import es_sm100_mxfp8_blockscaled_grouped_quant, es_sm100_mxfp8_blockscaled_grouped_mm
 
 def align(val: int, alignment: int = 128) -> int:
   return int((val + alignment - 1) // alignment * alignment)
@@ -25,57 +19,91 @@ def test_es_sm100_mxfp8_blockscaled_grouped_quant(num_experts, out_dtype):
   alignment = 128
   n_g = random.randint(1, 64) * alignment
   k_g = random.randint(1, 64) * alignment
-  # k_g = alignment
 
   expert_offset = 0
   expert_offsets = []
-  blockscale_offset = 0
-  blockscale_offsets = []
+  aux_expert_offset = 0
+  aux_expert_offsets = []
+  a_blockscale_offset = 0
+  a_blockscale_offsets = []
+  b_blockscale_offset = 0
+  b_blockscale_offsets = []
   problem_sizes = []
+  aux_problem_sizes = []
   a_list = []
   b_list = []
-  a_q_list = []
-  b_q_list = []
-  sfa_list = []
-  sfb_list = []
   ref_d_list = []
 
   for g in range(num_experts):
     m_g = random.randint(1, 4096)
-    # m_g = 128
     expert_offsets.append(expert_offset)
     expert_offset += m_g
-    blockscale_offsets.append(blockscale_offset)
-    blockscale_offset += align(m_g, 128)
+    aux_expert_offsets.append(aux_expert_offset)
+    aux_expert_offset += n_g
+    a_blockscale_offsets.append(a_blockscale_offset)
+    a_blockscale_offset += align(m_g, 128)
+    b_blockscale_offsets.append(b_blockscale_offset)
+    b_blockscale_offset += n_g  # n_g already align to 128
     problem_sizes.append([m_g, n_g, k_g])
+    aux_problem_sizes.append([n_g, m_g, k_g])
 
     a = torch.randn((m_g, k_g), device=device, dtype=out_dtype)  # (M, K):(K, 1)
-    a_q, sfa = torch.ops.trtllm.mxfp8_quantize(a, True, alignment=128)
+    b = torch.randn((n_g, k_g), device=device, dtype=out_dtype)  # (N, K):(K, 1)
+    
     a_list.append(a)
-    a_q_list.append(a_q)
-    sfa_list.append(sfa)
+    b_list.append(b)
+    ref_d = a @ b.T
+    ref_d_list.append(ref_d)
   a = torch.concat(a_list, dim=0)
-  a_q = torch.concat(a_q_list, dim=0)
-  sfa = torch.concat(sfa_list, dim=0)
+  b = torch.concat(b_list, dim=0)
 
   _problem_sizes = torch.tensor(problem_sizes).to(device=device, dtype=torch.int32)
+  _aux_problem_sizes = torch.tensor(aux_problem_sizes).to(device=device, dtype=torch.int32)
   _expert_offsets = torch.tensor(expert_offsets).to(device=device, dtype=torch.int32)
-  _blockscale_offsets = torch.tensor(blockscale_offsets).to(device=device, dtype=torch.int32)
-  a_quant = torch.zeros_like(a_q, device=device)
-  scale_factor = torch.zeros_like(sfa, device=device)
+  _aux_expert_offsets = torch.tensor(aux_expert_offsets).to(device=device, dtype=torch.int32)
+  _a_blockscale_offsets = torch.tensor(a_blockscale_offsets).to(device=device, dtype=torch.int32)
+  _b_blockscale_offsets = torch.tensor(b_blockscale_offsets).to(device=device, dtype=torch.int32)
+
+  a_quant = torch.zeros_like(a, device=device)
+  a_scale_factor = torch.zeros((expert_offset, k_g // 32), dtype=torch.uint8, device=device)
+
+  b_quant = torch.zeros_like(b, device=device)
+  b_scale_factor = torch.zeros((num_experts, n_g, k_g // 32), dtype=torch.uint8, device=device)
+
   es_sm100_mxfp8_blockscaled_grouped_quant(
     a,
     _problem_sizes,
     _expert_offsets,
-    _blockscale_offsets,
+    _a_blockscale_offsets,
     a_quant,
-    scale_factor
+    a_scale_factor
   )
-  print(a_q)
-  print(a_quant)
-  print(torch.sum(scale_factor - sfa))
-  print(scale_factor)
-  print(sfa)
+
+  es_sm100_mxfp8_blockscaled_grouped_quant(
+    b,
+    _aux_problem_sizes,
+    _aux_expert_offsets,
+    _b_blockscale_offsets,
+    b_quant,
+    b_scale_factor
+  )
+  b_quant = b_quant.view(num_experts, n_g, k_g).transpose(1, 2)
+  b_scale_factor= b_scale_factor.view(num_experts, n_g, k_g // 32).transpose(1, 2)
+
+  d = torch.empty((expert_offset, n_g), device=device, dtype=out_dtype)
+  es_sm100_mxfp8_blockscaled_grouped_mm(
+    d, a_quant, b_quant, a_scale_factor, b_scale_factor, _problem_sizes, _expert_offsets, _a_blockscale_offsets
+  )
+
+  for g in range(num_experts):
+    baseline = ref_d_list[g]
+    actual = d[expert_offsets[g] : (expert_offsets[g] + problem_sizes[g][0])]
+    diff = calc_diff(actual, baseline)
+    assert diff < 0.001
+    print(f"m_g={baseline.shape[0]} n_g={n_g} k_g={k_g} num_experts={num_experts}, out_dtype={out_dtype}, diff={diff:.5f}: OK")
 
 if __name__ == '__main__':
-  test_es_sm100_mxfp8_blockscaled_grouped_quant(2, torch.bfloat16)
+  test_es_sm100_mxfp8_blockscaled_grouped_quant(8, torch.bfloat16)
+  test_es_sm100_mxfp8_blockscaled_grouped_quant(8, torch.float16)
+  test_es_sm100_mxfp8_blockscaled_grouped_quant(16, torch.bfloat16)
+  test_es_sm100_mxfp8_blockscaled_grouped_quant(16, torch.float16)
