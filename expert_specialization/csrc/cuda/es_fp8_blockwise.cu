a31f031f285c906d2411ca066238d4e1b2f310b1
戚余航 <qiyuhang@bytedance.com>
2025-12-14 22:38:10 +0800

    Fix.

--- expert_specialization/csrc/cuda/es_fp8_blockwise.cu
+++ expert_specialization/csrc/cuda/es_fp8_blockwise.cu
@@ -186,9 +186,9 @@ void es_fp8_blockwise_scaled_grouped_mm(
 
   if (!is_h20_device) {
     end_event_0.recordOnce(backup_stream_0);
-    end_event_1.recordOnce(backup_stream_1);
+    // end_event_1.recordOnce(backup_stream_1);
     end_event_0.block(stream);
-    end_event_1.block(stream);
+    // end_event_1.block(stream);
   }
 #else
   TORCH_CHECK_NOT_IMPLEMENTED(
